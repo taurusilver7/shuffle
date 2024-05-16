12d0d351d155d694970a525525073e4dc123d89b
Devi prasad <prasad20697@gmail.com>
2024-05-16 11:36:25 +0530

    misc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				mainBackground: "#0D1117",
+				columnBackground: "#161c22",
+			},
+		},
+	},
+	plugins: [],
+};
