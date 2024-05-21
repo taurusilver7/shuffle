8618f1f2614c9cad2687d7221530b18b215782dc
Devi prasad <prasad20697@gmail.com>
2024-05-21 12:37:51 +0530

    column delete added

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import PlusIcon from "./plus";
-import { Column } from "../types";
+import { Column, Id } from "../types";
+import ColumnContainer from "./column-container";
 
 const Board = () => {
 	const [columns, setColumns] = useState<Column[]>([]);
@@ -19,25 +20,38 @@ const Board = () => {
 		return Math.floor(Math.random() * 10001);
 	};
 
+	const deleteColumn = (id: Id) => {
+		const filteredColumn = columns.filter((col) => col.id !== id);
+		setColumns(filteredColumn);
+	};
+
 	return (
-		<div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-10">
-			<div className="m-auto flex gap-4">
-				<div className="flex gap-2">
+		<div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden no-scrollbar px-10">
+			<div className="mx-auto flex flex-col gap-y-4">
+				<div className="fixed top-6 left-10 flex justify-between items-center">
+					<button
+						onClick={() => {
+							createNewColumn();
+						}}
+						className="flex gap-2 h-[60px] w-[350px] min-w-[350px] rounded-lg bg-mainBackground border-2 border-columnBackground p-4 ring-rose-400 hover:ring-2"
+					>
+						<PlusIcon />
+						Add Column
+					</button>
+
+					<div className="pl-12">
+						{columns.length > 0 && `${columns.length} sections`}
+					</div>
+				</div>
+				<div className="flex gap-2 mt-10">
 					{columns.map((col, index) => (
-						<div key={index} className="">
-							<div>{col.title}</div>
-						</div>
+						<ColumnContainer
+							key={index}
+							column={col}
+							deleteColumn={deleteColumn}
+						/>
 					))}
 				</div>
-				<button
-					onClick={() => {
-						createNewColumn();
-					}}
-					className=" flex gap-2 h-[60px] w-[350px] min-w-[350px] rounded-lg bg-mainBackground border-2 border-columnBackground p-4 ring-rose-400 hover:ring-2"
-				>
-					<PlusIcon />
-					Add Column
-				</button>
 			</div>
 		</div>
 	);
